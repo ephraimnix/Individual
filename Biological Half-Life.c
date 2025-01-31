5a3c3867efc934f1bc5f004e558b288b52d678bf
ephraimnix <bonanmarcephraim@gmail.com>
2025-01-31 19:06:37 +0100

    Create Biological Half-Life.c

---
+++ Biological Half-Life.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+/* Introduire plusieurs variables ;
+la dose initiale ingérée et l'unité de mesure
+la demie vie du medicament en h
+temps écoulé en h
+dose restante
+*/ 
