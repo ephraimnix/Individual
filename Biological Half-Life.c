647a8c143492be8c5dfbcc9c21c0df6fa617e8db
ephraimnix <bonanmarcephraim@gmail.com>
2025-02-03 18:23:41 +0100

    Update Biological Half-Life.c

--- Biological Half-Life.c
+++ Biological Half-Life.c
@@ -2,9 +2,23 @@
 #include <string.h>
 #include <math.h>
 
-/* Introduire plusieurs variables ;
-la dose initiale ingérée et l'unité de mesure
-la demie vie du medicament en h
-temps écoulé en h
-dose restante
-*/ 
+int main()
+{
+    double dose; 
+    double demie_vie; 
+    char unites[2];
+    double temps_ecoule;  
+    double dose_instant; 
+    printf("Entrez la dose de médicament : ");
+    scanf("%lf", &dose);
+    printf("Entrez l'unité de la dose (mg, μg et g) : ");
+    scanf("%s", unite);
+
+//On demande à l'utilisateur quel est le temps écoulé depuis l'heure de orise (temps_ecouble)
+
+    printf("Entrer la demi-vie en heure(s) : ");
+    scanf("%lf", &demi_Vie);
+
+/*** A compléter*
+     ***/
+}
