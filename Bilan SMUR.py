a00a6f8f44f35807be4fd7c92011a9afa71d10ef
ephraimnix <bonanmarcephraim@gmail.com>
2025-02-18 01:23:12 +0100

    Create Bilan SMUR.py

---
+++ Bilan SMUR.py
@@ -0,0 +1,4 @@
+#Bilan circonstanciel, plusieurs questions, saisies enregistrées dans une variable
+#Idem pour le bilan cardiaque, respiratoire et neurologique
+#Possibilité d'aller plus loin avec un examen plus poussé après une question du programme
+#ATCD, traitements etc..
